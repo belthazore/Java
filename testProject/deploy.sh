4c7f6d005435c2523dbf4e2ab8b878d67cd8d457
I g n i s <you@example.com>
2018-07-01 19:03:27 +0300

    1. removed servletsilent.run.sh; 2. fixed deploy.sh

--- testProject/deploy.sh
+++ testProject/deploy.sh
@@ -1,5 +1,5 @@
-mvn package
-sudo unzip -o /home/nnm/IdeaProjects/testProject/target/project-1.0.war -d /opt/tomcat/webapps/project/
+mvn clean package
+sudo unzip -o target/project-1.0.war -d /opt/tomcat/webapps/project/
 sudo rm -r -f /opt/tomcat/webapps/project/META-INF
 sudo chown -R tomcat:tomcat /opt/tomcat/webapps/project/WEB-INF
 sudo systemctl restart tomcat
