2f85c48b1a4dc2d31e96445f42b1891a22c43d8a
nnm <belthazore@ya.ru>
2018-08-02 12:58:52 +0300

    cleared

--- testProject/deploy.sh
+++
@@ -1,6 +0,0 @@
-mvn clean package
-sudo unzip -o target/project-1.0.war -d /opt/tomcat/webapps/project/
-sudo rm -r -f /opt/tomcat/webapps/project/META-INF
-sudo chown -R tomcat:tomcat /opt/tomcat/webapps/project
-sudo systemctl restart tomcat
-echo ''
