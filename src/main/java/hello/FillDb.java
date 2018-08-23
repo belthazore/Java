2426f4b06bf4990ff59e75b7fb6822fc31e0efe9
nnm <belthazore@ya.ru>
2018-08-23 19:04:59 +0300

    added fillDb and routing

---
+++ src/main/java/hello/FillDb.java
@@ -0,0 +1,45 @@
+package hello;
+
+import java.security.SecureRandom;
+
+import static java.lang.System.out;
+
+public class FillDb {
+
+    // Запись N-строк в PostgreSQL
+    static void start(int count) {
+        String tableName = "contacts2";
+        StringBuilder QUERY = new StringBuilder().append("INSERT INTO " + tableName + "(name) " +
+                "VALUES ('" + (getRandString(getRandOneInt()) + " " + getRandString(getRandOneInt())) + "')");
+
+        int countPg = 0;
+        while (countPg < count) {
+            String fnameLname = getRandString(getRandOneInt()) + " " + getRandString(getRandOneInt());
+            QUERY.append(", ('" + fnameLname + "') ");
+            countPg++;
+            if (String.valueOf(countPg).split("0").length > 1)
+                out.println(countPg);
+        }
+        jdbcPostgres.execute2(QUERY.toString());
+    }
+
+    // w.o. zero (!)
+    private static int getRandOneInt() {
+        String AB = "456789";
+        return Integer.parseInt(getRandString(1, AB));
+    }
+
+    private static String getRandString(int length) {
+//        String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        String AB = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        return getRandString(length, AB);
+    }
+
+    private static String getRandString(int length, String AB) {
+        SecureRandom rnd = new SecureRandom();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++)
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+        return sb.toString();
+    }
+}
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,15 +1,14 @@
 package hello;
 
+import org.springframework.web.bind.annotation.*;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.springframework.web.bind.annotation.*;
 
 import static java.lang.System.out;
 
@@ -22,16 +21,17 @@ public class GreetingController {
 
     //REST
     @RequestMapping(value = "/rest", method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody MyList rest(@RequestParam(value="nameFilter", required=true) String nameFilter) {
+    public @ResponseBody
+    MyList rest(@RequestParam(value = "nameFilter", required = true) String regExpr) {
         try {
-            contactsList.add(new Greeting(nameFilter));
+            contactsList.add(new Greeting(regExpr));
         } catch (Exception e) { e.printStackTrace(); }
 
         // ---------------------------------------------------------------
         ResultSet rs = jdbcPostgres.select("SELECT * FROM contacts;", new String[]{});
         int foundCount = 0; // Количество найденных
 //        String regExpr = "[^A].*";
-        String regExpr = nameFilter;
+        out.println("Received 'regExpr': " + regExpr);
         Pattern pattern = Pattern.compile(regExpr);
         Matcher match;
         try {
@@ -39,8 +39,8 @@ public class GreetingController {
                 while (rs.next() && foundCount < 10) {
                     String clientName = rs.getString(2);
                     match = pattern.matcher(clientName);
-                    if (!match.find()) { // Паттерн совпал, найдено имя todo переделать наоборот (ТЗ)
-                        System.out.println("true: " + clientName);
+                    if (match.find()) { // Паттерн совпал, найдено имя todo переделать наоборот (ТЗ)
+                        out.println("true: " + clientName);
                         contactsList.add(new Greeting(clientName));
                         foundCount++;
                     }
@@ -53,10 +53,19 @@ public class GreetingController {
     }
 
     @RequestMapping(value = "/hello/contacts", method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody Obj contacts(@RequestParam(value="nameFilter", required=true) String name) {
+    public @ResponseBody
+    Obj contacts(@RequestParam(value = "nameFilter", required = true) String name) {
         contactsList.add( new Greeting(name) );
 
-        System.out.println(Arrays.toString(contactsList.toArray()));
+        return new Obj(contactsList, contactsList.size());
+    }
+
+    @RequestMapping(value = "/fillDb", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody
+    Obj fillDb(@RequestParam(value = "count", required = true) int count) {
+        contactsList.add(new Greeting(String.valueOf(count)));
+
+        FillDb.start(count);
         return new Obj(contactsList, contactsList.size());
     }
 }
